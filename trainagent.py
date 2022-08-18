ed58b50aab40e8f68fce6b6c51baaa48f201d665
steve <s.mander3@lancaster.ac.uk>
2022-08-18 13:20:12 +0100

    call prepare

--- trainagent.py
+++ trainagent.py
@@ -27,6 +27,7 @@ preprocess=Compose([
         Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
         ])
 Dataset=COCODataModule(Cache_dir=p.data_dir,T=preprocess)
+Dataset.prepare_data()
 assert len(Dataset)>0,"Dataset is empty"
 train=partial(train,dir=p.data_dir,devices=p.devices, accelerator=p.accelerator,Dataset=Dataset)
 wandb.agent(sweep_id=p.sweep_id, project="6DimContrSweep", entity="st7ma784",function=train)
