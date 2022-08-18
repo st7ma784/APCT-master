baa5de5ca9f444a2edd2524368c2d6ea0d6f1776
steve <s.mander3@lancaster.ac.uk>
2022-08-18 13:28:34 +0100

    remove assert

--- trainagent.py
+++ trainagent.py
@@ -29,6 +29,5 @@ preprocess=Compose([
 Dataset=COCODataModule(Cache_dir=p.data_dir,T=preprocess)
 Dataset.prepare_data()
 Dataset.setup()
-assert len(Dataset)>0,"Dataset is empty"
 train=partial(train,dir=p.data_dir,devices=p.devices, accelerator=p.accelerator,Dataset=Dataset)
 wandb.agent(sweep_id=p.sweep_id, project="6DimContrSweep", entity="st7ma784",function=train)
