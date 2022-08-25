748110af84313c4ed422a96745783c19a0575b71
steve <s.mander3@lancaster.ac.uk>
2022-08-25 16:51:12 +0100

    fix agent

--- trainagent.py
+++ trainagent.py
@@ -6,8 +6,7 @@
 import wandb
 from trainclip_v2 import train
 from BuildSpainDataSet import COCODataModule
-from torchvision.transforms import Compose, Resize, CenterCrop, ToTensor, Normalize
-from PIL import Image
+
 import argparse
 import wandb
 from functools import partial
@@ -19,14 +18,8 @@ parser.add_argument("--devices", default="auto",nargs="?", type=str)
 parser.add_argument("--accelerator", default="auto",nargs="?", type=str)
 
 p = parser.parse_args()
-preprocess=Compose([
-        Resize(224, interpolation=Image.BICUBIC),
-        CenterCrop(224),
-        lambda image: image.convert("RGB"),
-        ToTensor(),
-        Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
-        ])
-Dataset=COCODataModule(Cache_dir=p.data_dir,T=preprocess)
+
+Dataset=COCODataModule(Cache_dir=p.data_dir)
 Dataset.prepare_data()
 Dataset.setup()
 train=partial(train,dir=p.data_dir,devices=p.devices, accelerator=p.accelerator,Dataset=Dataset)
