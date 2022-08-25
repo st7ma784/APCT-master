927baecce376af2e3d373e74369e5c98b5a76099
steve <s.mander3@lancaster.ac.uk>
2022-08-25 17:24:16 +0100

    add Bede add

---
+++ SlurmTrain.py
@@ -0,0 +1,54 @@
+from test_tube import SlurmCluster
+from trainclip_v2 import train
+from HOparser import parser
+if __name__ == '__main__':
+   
+    argsparser = parser(strategy='random_search')
+    hyperparams = argsparser.parse_args()
+
+    # Enable cluster training.
+    cluster = SlurmCluster(
+        hyperparam_optimizer=hyperparams,
+        log_path=hyperparams.log_path,
+        python_cmd='python3',
+        test_tube_exp_name=hyperparams.test_tube_exp_name
+    )
+
+    # Email results if your hpc supports it.
+    cluster.notify_job_status(
+        email='st7ma784@gmail.com', on_done=True, on_fail=True)
+
+    # SLURM Module to load.
+    # cluster.load_modules([
+    #     'python-3',
+    #     'anaconda3'
+    # ])
+
+    # Add commands to the non-SLURM portion.
+    
+    cluster.add_command('source activate open-ce') # We'll assume that on the BEDE/HEC cluster you've named you conda env after the standard...
+
+    # Add custom SLURM commands which show up as:
+    # #comment
+    # #SBATCH --cmd=value
+    # ############
+    # cluster.add_slurm_cmd(
+    #    cmd='cpus-per-task', value='1', comment='CPUS per task.')
+
+    # Set job compute details (this will apply PER set of hyperparameters.)
+    cluster.per_experiment_nb_gpus = 1
+    cluster.per_experiment_nb_nodes = 1
+    #cluster.gpu_type = '1080ti'
+
+    # we'll request 40GB of memory per node
+    cluster.memory_mb_per_node = 50000
+
+    # set a walltime of 24 hours,0, minues
+    cluster.job_time = '24:00:00'
+
+    # 1 minute before walltime is up, SlurmCluster will launch a continuation job and kill this job.
+    # you must provide your own loading and saving function which the cluster object will call
+    cluster.minutes_to_checkpoint_before_walltime = 1
+
+    # run the models on the cluster
+    cluster.optimize_parallel_cluster_gpu(train, nb_trials=40, job_name='first_trial_batch', job_display_name='my_BEDETestSweep') # Change this to optimize_parralel_cluster_cpu to debug.
