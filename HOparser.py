f98fbea6497fd8eff29574a62eb54dd3e395c703
John Doe <johndoe@example.com>
2022-10-08 23:53:07 +0100

    fix run command

--- HOparser.py
+++ HOparser.py
@@ -5,7 +5,7 @@ class parser(HyperOptArgumentParser):
 
         super().__init__( *args,strategy=strategy, add_help=False) # or random search
         #more info at https://williamfalcon.github.io/test-tube/hyperparameter_optimization/HyperOptArgumentParser/
-        self.opt_list("--log_path",default="/Data",type=str,tunable=False)
+        self.opt_list("--log_path",default="/nobackup/projects/$USER/smander3/logs/",type=str,tunable=False)
         self.opt_list("--learning_rate", default=0.001, type=float, options=[2e-4,1e-4,5e-5,2e-5,1e-5,4e-6], tunable=True)
         self.opt_list("--batch_size", default=8, type=float, options=[1,2,4,8,12,16, 32, 64], tunable=True)
         self.opt_list("--JSE", default=True, type=bool, options=[True,False], tunable=True)
