aba0511d4800f70c55fcfbda36c0f56abd64e860
John Doe <johndoe@example.com>
2022-10-09 01:57:29 +0100

    fix v2 train wandb

--- trainclip_v2.py
+++ trainclip_v2.py
@@ -204,6 +204,8 @@ def testtrainfunc(config=None,dir="/Data",devices="auto",accelerator="auto",Data
     with wandb.init(project="BEDETEST",entity="st7ma784",name="BEDETEST",config=config) as run:
         run.log({"test":1})  # only log first rank
 def wandbtrain(config=None,dir="/Data",devices="auto",accelerator="auto",Dataset=None):
+    if not isinstance(config,dict):
+        config=config.__dict__
     with wandb.init(project="6DIMContrSweep",entity="st7ma784",name="6DIMContrSweep",config=config) as run:
 
         logtool= pytorch_lightning.loggers.WandbLogger( name="BEDEContrSweep",project="6DIMContrSweep",entity="st7ma784",experiment=run, save_dir=dir)
