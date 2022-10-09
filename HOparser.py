f1406baa70f3331190317d908c6f9fcaf4166475
John Doe <johndoe@example.com>
2022-10-09 02:44:47 +0100

    reduce opts

--- HOparser.py
+++ HOparser.py
@@ -7,10 +7,10 @@ class parser(HyperOptArgumentParser):
         #more info at https://williamfalcon.github.io/test-tube/hyperparameter_optimization/HyperOptArgumentParser/
         self.add_argument("--dir",default="/nobackup/projects/$USER/data",type=str)
         self.add_argument("--log_path",default="/nobackup/projects/$USER/logs/",type=str)
-        self.opt_list("--learning_rate", default=0.001, type=float, options=[2e-4,1e-4,5e-5,2e-5,1e-5,4e-6], tunable=True)
-        self.opt_list("--batch_size", default=8, type=float, options=[1,2,4,8,12,16, 32, 64], tunable=True)
+        self.opt_list("--learning_rate", default=0.001, type=float, options=[2e-4,1e-4,5e-5,1e-5,4e-6], tunable=True)
+        self.opt_list("--batch_size", default=8, type=float, options=[8,12,16, 20,24], tunable=True)
         self.opt_list("--JSE", default=True, type=bool, options=[True,False], tunable=True)
-        self.opt_list("--precision", default=16, options=[32,16,'bf16'], tunable=False)
+        self.opt_list("--precision", default=32, options=[32,'bf16'], tunable=False)
         #self.opt_range('--neurons', default=50, type=int, tunable=True, low=100, high=800, nb_samples=8, log_base=None)
 
 
