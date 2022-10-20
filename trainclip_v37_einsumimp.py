c2b9035505e256f2620fc4ed6a8d797aaa98e986
steve <s.mander3@lancaster.ac.uk>
2022-10-20 14:37:43 +0100

    fix autoselect

--- trainclip_v37_einsumimp.py
+++ trainclip_v37_einsumimp.py
@@ -482,7 +482,8 @@ def train(config={
         p=16 if p=="bf16" else int(p)  ##needed for BEDE
     print("Launching with precision",p)
     trainer=pytorch_lightning.Trainer(
-            devices="auto",
+            devices=devices,
+            auto_select_gpus=True,
             accelerator=accelerator,
             max_epochs=40,
             #profiler="advanced",
