2ce1bc0e31abaf8a95d8bc41b37165df1a16faed
John Doe <johndoe@example.com>
2022-11-18 10:44:12 +0000

    fix

--- modelVersions/trainclip_v46_var.py
+++ modelVersions/trainclip_v46_var.py
@@ -216,9 +216,9 @@ class LightningCLIPModule(LightningModule):
         self._insert_hooks()
         self.eval()
         self.classifier = LogisticRegression(random_state=0, C=0.316, max_iter=1000, verbose=1)
-
+       
         if len(self.features)>0:
-            features=torch.cat(self.features,dim=0).cpu().numpy()
+            features=torch.nan_to_num(torch.cat(self.features,dim=0).cpu().numpy())
             labels=torch.cat(self.labels,dim=0).cpu().numpy()
             self.classifier.fit(features, labels)
         self.Linearloss=0
