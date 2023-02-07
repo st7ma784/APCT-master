f26a2962b38d677cafe847ecbb7e6ae15f46c722
steve <s.mander3@lancaster.ac.uk>
2023-02-07 15:50:38 +0000

    fx

--- modelVersions/trainclip_v48_Prune.py
+++ modelVersions/trainclip_v48_Prune.py
@@ -536,6 +536,8 @@ class PruneHook(EntropyHook):
             for module_name, module in filter(lambda item : type(item[1]) in self.activations, block.named_modules()):
                 im_score = compute_importance(module.weight.detach(), entropy[module_name], eta)
                 prune_module(module,module_name, im_score, self.args)
+                #now consider pruning the near by batch norm layers 
+        
         
         '''
                 
