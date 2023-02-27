d17215415ea0528fefec45f04b536d6fab3e1e1e
steve <you@example.com>
2023-02-27 15:50:43 +0000

    fix validations

--- model/PruneCalculation.py
+++ model/PruneCalculation.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import torch
+import pytorch_lightning as pl
 from functools import partial
 import sys
 sys.path.append("./APCT-master")
@@ -9,6 +10,43 @@ from core.pattern import EntropyHook
 from functools import partial
 from random import random
 from collections import defaultdict
+
+import inspect
+import logging
+from copy import deepcopy
+from functools import partial
+from typing import Any, Callable, Dict, List, Optional, Sequence, Tuple, Union
+
+import torch.nn.utils.prune as pytorch_prune
+from lightning_utilities.core.apply_func import apply_to_collection
+from torch import nn, Tensor
+from typing_extensions import TypedDict
+
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks.callback import Callback
+from pytorch_lightning.core.module import LightningModule
+from pytorch_lightning.utilities.exceptions import MisconfigurationException
+from pytorch_lightning.utilities.rank_zero import rank_zero_debug, rank_zero_only
+
+log = logging.getLogger(__name__)
+
+_PYTORCH_PRUNING_FUNCTIONS = {
+    "ln_structured": pytorch_prune.ln_structured,
+    "l1_unstructured": pytorch_prune.l1_unstructured,
+    "random_structured": pytorch_prune.random_structured,
+    "random_unstructured": pytorch_prune.random_unstructured,
+}
+
+_PYTORCH_PRUNING_METHOD = {
+    "ln_structured": pytorch_prune.LnStructured,
+    "l1_unstructured": pytorch_prune.L1Unstructured,
+    "random_structured": pytorch_prune.RandomStructured,
+    "random_unstructured": pytorch_prune.RandomUnstructured,
+}
+
+_PARAM_TUPLE = Tuple[nn.Module, str]
+_PARAM_LIST = Sequence[_PARAM_TUPLE]
+_MODULE_CONTAINERS = (LightningModule, nn.Sequential, nn.ModuleList, nn.ModuleDict)
 
 class PruneHook(EntropyHook):
     def __init__(self, model, Gamma, ratio=1, **kwargs):
@@ -136,3 +174,56 @@ def prune_Residual_Attention_block(block, block_entropy, eta):
         (K,"weight",block_type):V for K,V in lt_importance_dict.items()}
     
     return linear_im_dict
+
+
+class myPruneCallback(pl.callbacks.ModelPruning):
+
+    def __init__(self,Gamma,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.activations =set([nn.Linear])#set([nn.LeakyReLU, nn.ReLU, nn.ELU, nn.Sigmoid, nn.GELU,QuickGELU, nn.Tanh, nn.PReLU])
+        self.Gamma=torch.tensor(Gamma, dtype=torch.float32)
+
+    def hook(self, layer, input_var, output_var, layer_name):
+        #if random()>self.ratio:# here because adds random noise to the data
+        input=output_var.view(output_var.shape[-1],-1)
+        hist=torch.bucketize(input, self.Gamma)# returns index of gamma to each value.
+        counts=torch.nn.functional.one_hot(hist, self.Gamma.shape[0]+1).sum(dim=1)
+
+        #counts=torch.bincount(hist,minlength=self.Gamma.shape[0]+1)
+        self.features[layer_name]= counts.add(self.features[layer_name])
+
+
+    def on_train_epoch_start(self) -> None:
+        self.PruneHandles=[]
+        self.features=defaultdict(lambda: torch.zeros((1,self.Gamma.shape[0]+1), dtype=torch.float32, device=self.device))
+        self.PruneHandles.extend( [module.register_forward_hook(partial(self.hook, layer_name=module_name)) for module_name, module in self.model.named_modules() if type(module) in self.activations])
+
+    def process_layer(self,layer):
+        layer = layer.reshape(self.Gamma.shape[0]+1, -1)
+        layer /= layer.sum(axis=0)
+        return torch.sum(-layer*torch.log(1e-8+layer),dim=0) # changed from 0 
+
+    def process_block_entropy(self, blockdict):
+        if len(blockdict)==0:
+            return torch.zeros(1)
+        return {k:self.process_layer(v) for k,v in blockdict.items()}
+
+    def retrieve(self,eta=-1):
+        if len(self.features.keys())==0:
+            return {}
+        entropy=self.process_block_entropy(self.features) 
+        print("entropy",entropy.keys())
+
+        #for block_name, block in self.model.named_modules():
+        for (module_name, module) in filter(lambda item : type(item[1]) in self.activations and random()<self.ratio, self.model.named_modules()):
+            im_score = compute_importance(module.weight.detach(), entropy[module_name], eta)
+            prune_module(module,"weight", im_score, self.args)
+            #now consider pruning the near by batch norm layers 
+    def on_train_epoch_end(self) -> None:
+        for hook in self.pruneHooks:
+            hook.remove()
+    def on_validation_epoch_end(self):
+        for hook in self.pruneHooks:
+            global_entropy = hook.retrieve()
+            hook.remove()        
+
